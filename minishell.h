fd0ea0340d08b021a15ad30205c491d437d1ecf0
Titouan Autin--Desbois <tautin--@paul-f3Ar4s6.clusters.42paris.fr>
2025-03-05 17:48:18 +0100

    tout tout debut ebauche de .h et main

---
+++ minishell.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tautin-- <tautin--@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 17:04:11 by tautin--          #+#    #+#             */
+/*   Updated: 2025/03/05 17:09:05 by tautin--         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/****************/
+/* Lib includes */
+/****************/
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+
+
+#endif
